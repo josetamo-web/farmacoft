6c649e6ce9dffee5d1a088e4ec33acfc87cee94a
josetamo-web <jose.tamo@uab.edu.bo>
2026-07-30 21:47:44 -0400

    se agrego un boton para el cambio de modo dia a noche

---
+++ script.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+const botonmodo = document.querySelector("#btn-tema");
+
+let esdedia = true;
+
+const hora = new Date().getHours();
+
+if (hora >= 6 && hora < 18) {
+    body.classList.add("claro");
+    esdedia = true;
+    botonmodo.textContent = "modo noche";
+} else {
+    body.classList.add("oscuro");
+    esdedia = false;
+    botonmodo.textContent = "modo día";
+}
+
+function alternarmodo() {
+    body.classList.toggle("claro");
+    esdedia = !esdedia;
+
+    if (esdedia) {
+        body.classList.remove("oscuro");
+        body.classList.add("claro");
+        botonmodo.textContent = "modo noche";
+    } else {
+        body.classList.remove("claro");
+        body.classList.add("oscuro");
+        botonmodo.textContent = "modo día";
+    }
+}
+botonmodo.addEventListener("click", alternarmodo);
